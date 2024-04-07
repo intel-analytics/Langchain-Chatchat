9d3ab04253412de3710a8f6513e3ba5f868d3960
Huang, Xinshengzi <xinshengzi.huang@intel.com>
2024-04-07 15:52:51 +0800

    modify log in server/model_workers/qianfan

--- server/model_workers/qianfan.py
+++ server/model_workers/qianfan.py
@@ -140,7 +140,7 @@ class QianFanWorker(ApiModelWorker):
                                 "code": None,
                             }
                         }
-                        self.logger.error(f"An error occurred when requesting the Qianfan API: {data}")
+                        self.logger.error(f"An error occurred when requesting the qianfan API: {data}")
                         yield data
 
     def do_embeddings(self, params: ApiEmbeddingsParams) -> Dict:
@@ -179,7 +179,7 @@ class QianFanWorker(ApiModelWorker):
                             "code": None,
                         }
                     }
-                    self.logger.error(f"An error occurred when requesting the Qianfan API: {data}")
+                    self.logger.error(f"An error occurred when requesting the qianfan API: {data}")
                     return data
                 else:
                     embeddings = [x["embedding"] for x in resp.get("data", [])]
